c6939e817b1f37e747e7a2e5cfa49877dfc13974
Lan Liang <gcslyp@gmail.com>
2023-04-12 16:36:22 +0800

    u

--- hack/docker.sh
+++ hack/docker.sh
@@ -66,7 +66,23 @@ function build_local_image() {
 
   if [[ "$output_type" == "registry" ]]; then
     docker push "${image_name}"
+    cosignImage "${image_name}"
   fi
+}
+
+function cosignImage(){
+
+echo "====================begin cosign for :"$1
+
+echo "github.sha:" ${{ github.sha }}
+echo "github.run_id:" ${{ github.run_id }}
+echo "github.run_attempt:" ${{ github.run_attempt }}
+
+cosign sign --yes \
+            -a sha=${{ github.sha }} \
+            -a run_id=${{ github.run_id }} \
+            -a run_attempt=${{ github.run_attempt }} \
+            $1
 }
 
 function build_cross_image() {
