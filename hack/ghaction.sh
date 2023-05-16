bd7fc54327a92b410a459a6bbc2ebb79d33e0ee9
Lan Liang <gcslyp@gmail.com>
2023-05-16 15:51:18 +0800

    u

--- hack/ghaction.sh
+++ hack/ghaction.sh
@@ -52,6 +52,9 @@ if [ -z "$ISSUE_COMMENT" ];then
     exit -1
 fi
 
+echo "printing issue coment:"
+echo -e $ISSUE_COMMENT
+
 echo -e $ISSUE_COMMENT |sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'| while read line
 do
   if [[ "$line" == "/retest-faile" ]]; then
