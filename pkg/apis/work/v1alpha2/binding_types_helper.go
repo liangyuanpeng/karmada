6dc0e9a1be276f7d2b96b78aa60bfa7d08162c09
Lan Liang <gcslyp@gmail.com>
2023-06-27 15:35:59 +0800

    Optimize: check graceful eviction tasks before cut clusters for target cluster
    Signed-off-by: Lan Liang <gcslyp@gmail.com>

--- pkg/apis/work/v1alpha2/binding_types_helper.go
+++ pkg/apis/work/v1alpha2/binding_types_helper.go
@@ -112,6 +112,11 @@ func (s *ResourceBindingSpec) RemoveCluster(name string) {
 // building a graceful eviction task.
 // This function no-opts if the cluster does not exist.
 func (s *ResourceBindingSpec) GracefulEvictCluster(name string, options *TaskOptions) {
+
+	// skip if the target cluster already in the task list
+	if s.ClusterInGracefulEvictionTasks(name) {
+		return
+	}
 	// find the cluster index
 	var i int
 	for i = 0; i < len(s.Clusters); i++ {
@@ -128,11 +133,6 @@ func (s *ResourceBindingSpec) GracefulEvictCluster(name string, options *TaskOpt
 
 	// remove the target cluster from scheduling result
 	s.Clusters = append(s.Clusters[:i], s.Clusters[i+1:]...)
-
-	// skip if the target cluster already in the task list
-	if s.ClusterInGracefulEvictionTasks(evictCluster.Name) {
-		return
-	}
 
 	// build eviction task
 	evictingCluster := evictCluster.DeepCopy()
