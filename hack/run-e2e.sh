3c0728b16f9c5c2e60a1cfa02ad49ad435b72bea
Lan Liang <gcslyp@gmail.com>
2023-08-17 09:29:56 +0000

    martix

--- hack/run-e2e.sh
+++ hack/run-e2e.sh
@@ -39,8 +39,10 @@ export KUBECONFIG=${KARMADA_APISERVER_KUBECONFIG}
 export PULL_BASED_CLUSTERS=${PULL_BASED_CLUSTERS}
 
 kubectl config get-contexts
+GINKGO_PARAM=${GINKGO_PARAM:-"--karmada-context=karmada-apiserver"}
 set +e
-ginkgo -v --race --trace --fail-fast -p --randomize-all ./test/e2e/ -- --karmada-context=karmada-apiserver --focus=\[operator\]
+ginkgo -v --race --trace --fail-fast -p --randomize-all ./test/e2e/ -- $GINKGO_PARAM
+#--karmada-context=karmada-apiserver --focus=\[operator\]
 TESTING_RESULT=$?
 
 # Collect logs
