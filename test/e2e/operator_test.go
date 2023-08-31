4f9faed056cf2406c0170b48bafab30e940d1825
Lan Liang <gcslyp@gmail.com>
2023-08-31 08:27:45 +0000

    u

--- test/e2e/operator_test.go
+++ test/e2e/operator_test.go
@@ -1,43 +1,45 @@
 package e2e
 
 import (
-	"context"
 	"fmt"
 	"os"
 
 	"github.com/onsi/ginkgo/v2"
 	appsv1 "k8s.io/api/apps/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/rand"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 
-	operatorv1alpha1 "github.com/karmada-io/karmada/operator/pkg/apis/operator/v1alpha1"
-	operator "github.com/karmada-io/karmada/operator/pkg/generated/clientset/versioned"
-
 	"github.com/karmada-io/karmada/test/e2e/framework"
 	"github.com/karmada-io/karmada/test/helper"
 )
 
-var _ = ginkgo.Describe("[operator] testing", func() {
+var _ = ginkgo.Describe("operator testing", func() {
 	var clusterClient kubernetes.Interface
 	homeDir := os.Getenv("HOME")
-	kubeConfigPath := fmt.Sprintf("%s/.kube/%s.config", homeDir, "karmada-host")
+	fmt.Println("==================begin operator testing")
+	kubeConfigPath := fmt.Sprintf("%s/.kube/%s.config", homeDir, "karmada")
 	fmt.Println("kubeConfigPath:", kubeConfigPath)
-	clusterConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
-	if err != nil {
-		panic(err)
-	}
-
-	operatorClient, _ := operator.NewForConfig(clusterConfig)
+	// clusterConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	restConfig, err = framework.LoadRESTClientConfig(kubeConfigPath, "karmada-host")
+	kubeClient, err = kubernetes.NewForConfig(restConfig)
+	// operatorClient, _ := operator.NewForConfig(clusterConfig)
 
 	ginkgo.BeforeEach(func() {
 		defaultConfigFlags := genericclioptions.NewConfigFlags(true).WithDeprecatedPasswordFlag().WithDiscoveryBurst(300).WithDiscoveryQPS(50.0)
 		defaultConfigFlags.Context = &karmadaContext
+		// kubeconfig = os.Getenv("KUBECONFIG")
+		// gomega.Expect(kubeconfig).ShouldNot(gomega.BeEmpty())
+		// clusterProvider = cluster.NewProvider()
+		// var err error
+		// restConfig, err = framework.LoadRESTClientConfig(kubeconfig, karmadaContext)
 	})
 
-	ginkgo.Context("Test namespaced resource: deployment", func() {
+	ginkgo.Context("[operator]Test namespaced resource: deployment", func() {
 		var deployment *appsv1.Deployment
 		var deploymentNamespace, deploymentName string
 
@@ -52,14 +54,18 @@ var _ = ginkgo.Describe("[operator] testing", func() {
 			framework.RemoveNamespace(kubeClient, deploymentNamespace)
 		})
 
-		ginkgo.It("test", func() {
+		ginkgo.It("[operator]test2", func() {
+			fmt.Println("hello")
+		})
+
+		ginkgo.It("[operator]test", func() {
 
 			ginkgo.By(fmt.Sprintf("Creating deployment %s with namespace %s ", deploymentName, deploymentNamespace), func() {
 				deploymentNamespaceObj := helper.NewNamespace(deploymentNamespace)
 				framework.CreateNamespace(clusterClient, deploymentNamespaceObj)
 				framework.CreateDeployment(clusterClient, deployment)
-				karmada := &operatorv1alpha1.Karmada{}
-				operatorClient.OperatorV1alpha1().Karmadas("default").Create(context.TODO(), karmada, v1.CreateOptions{})
+				// karmada := &operatorv1alpha1.Karmada{}
+				// operatorClient.OperatorV1alpha1().Karmadas("default").Create(context.TODO(), karmada, v1.CreateOptions{})
 			})
 		})
 
