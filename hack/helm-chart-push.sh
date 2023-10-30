82fff10a5efd9fa6fe526925d74c784e63884817
Lan Liang <gcslyp@gmail.com>
2023-10-30 09:17:29 +0000

    u

--- hack/helm-chart-push.sh
+++ hack/helm-chart-push.sh
@@ -6,8 +6,8 @@ set -o pipefail
 REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 source "${REPO_ROOT}/hack/util.sh"
 
-helm push _output/charts/karmada-chart-$1.tgz oci://docker.io/gcslyp
-helm push _output/charts/karmada-operator-chart-$1.tgz oci://docker.io/gcslyp
+helm push _output/charts/karmada-chart-$1.tgz oci://docker.io/lypgcs
+helm push _output/charts/karmada-operator-chart-$1.tgz oci://docker.io/lypgcs
 
-util::signImage docker.io/gcslyp/karmada:$1
-util::signImage docker.io/gcslyp/karmada-operator:$1
+util::signImage docker.io/lypgcs/karmada:$1
+util::signImage docker.io/lypgcs/karmada-operator:$1
